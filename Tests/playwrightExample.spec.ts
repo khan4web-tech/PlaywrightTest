0ed4a5c424099e5735c2dbacfa3de2cc09ee69fc
adnan <khan4web@gmail.com>
2026-06-04 18:29:31 +1000

    Add comments to test file

--- Tests/playwrightExample.spec.ts
+++ Tests/playwrightExample.spec.ts
@@ -1,6 +1,9 @@
 import { test } from '@playwright/test';
 
 test('open website and close browser', async ({ page }) => {
+  // Navigate to Google
   await page.goto('http://www.google.com');
+  
+  // Wait 3 seconds to see the page
   await page.waitForTimeout(3000);
 });
